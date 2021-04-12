6cb3caa4795402cae9da535c56deeb502fe42d3e
William Truppi <72492620+williamtruppi@users.noreply.github.com>
2021-04-12 16:45:44 +0200

    greeting change color with the time

--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ const date = new Date();
 const time = date.getHours();
 
 let greeting;
+let greetigStyle = {
+  color: ""
+};
 
 if (time < 12) {
   greeting = "Good Morning";
+  greetigStyle.color = "red";
 } else if (time < 18) {
   greeting = "Good Afternoon";
+  greetigStyle.color = "green";
 } else {
   greeting = "Good Night";
+  greetigStyle.color = "blue";
 }
 
-ReactDOM.render(<h1>{greeting}</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <h1 style={greetigStyle}>{greeting}</h1>,
+  document.getElementById("root")
+);
